09fa09668cead424191c8bab884d9af4d459ee12
inakigalan10 <inakigalandev@gmail.com>
2026-03-30 22:30:53 +0200

    updated title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Mi Portafolio</title>
+    <title>InakiGalanDev</title>
     @viteReactRefresh
     @vite(['resources/js/app.jsx', 'resources/css/app.css'])
     @inertiaHead
